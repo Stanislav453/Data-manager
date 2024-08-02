125e1e4306a5c67f65808ca9008e8653e92a8b68
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-02 08:32:59 +0200

    Create Item component, add button. Creating authentication for optional data.

---
+++ src/component/Item.tsx
@@ -0,0 +1,61 @@
+import { DataType } from "../type";
+import { CustomButton } from "./CustomButton";
+import { FaRegTrashCan, FaPencil } from "react-icons/fa6";
+
+type ItemProps = {
+  data: DataType[];
+};
+
+export const Item = ({ data }: ItemProps) => {
+  return data.map((oneResult, index) => {
+    const { name, gender, banned, age, type } = oneResult;
+
+    const isBanned = banned ? "Unbaned" : "banned";
+    const isBannedColor = banned ? "bg-green-500" : "bg-red-500";
+
+    return (
+      <li
+        key={index}
+        className={`flex w-full justify-between  items-center p-4  list-none ${
+          banned && "border-red-500"
+        } border-2 border-slate-300	 `}
+      >
+        <div className="flex  gap-2 w-full">
+          <h2>
+            <span className="font-bold"> Name: </span>
+            {name}
+          </h2>
+          {gender && (
+            <p>
+              <span className="font-bold"> Gender: </span>
+              {gender}
+            </p>
+          )}
+          {age && (
+            <p>
+              <span className="font-bold"> Age: </span>
+              {age}
+            </p>
+          )}
+          {type && (
+            <p>
+              <span className="font-bold"> Type: </span>
+              {type}
+            </p>
+          )}
+        </div>
+        <div className="flex">
+          <CustomButton>
+            <FaPencil className="text-3xl text-orange-500" />
+          </CustomButton>
+          <CustomButton>
+            <FaRegTrashCan className="text-3xl text-red-500" />
+          </CustomButton>
+          {gender && (
+            <CustomButton customStyle={isBannedColor}>{isBanned}</CustomButton>
+          )}
+        </div>
+      </li>
+    );
+  });
+};
