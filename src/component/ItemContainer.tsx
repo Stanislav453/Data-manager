276bb9189edb95551f929c83e7dda3c9db6756ef
Stanislav Jendrišák <st.jendrisak@gmail.com>
2024-08-02 21:27:12 +0200

    add form

--- src/component/ItemContainer.tsx
+++ src/component/ItemContainer.tsx
@@ -4,6 +4,9 @@ import { useGetAction } from "../api/actions/useGetAction";
 import { useAppData } from "../store/useAppData";
 import { Item } from "./Item";
 import { CustomButton } from "./CustomButton";
+// import { CreateInputForm } from './form/CreateItemForm';
+import { CreateUserForm } from './form/CreateUserForm';
+import { CreateAnimalForm } from './form/CreateAnimalForm';
 
 export const ItemContainer = () => {
   const [variant, setVariant] = useState("users");
@@ -13,22 +16,24 @@ export const ItemContainer = () => {
   console.log(appData);
 
   return (
-    <main className="w-full min-h-dvh py-5 px-3">
-      <div className="flex gap-3 py-3">
+    <main className='w-full min-h-dvh py-5 px-3'>
+      <div className='flex gap-3 py-3'>
         <CustomButton
-          action={() => setVariant("users")}
-          customStyle="bg-gray-500"
+          action={() => setVariant('users')}
+          customStyle='bg-gray-500'
         >
           Users
         </CustomButton>
         <CustomButton
-          action={() => setVariant("animals")}
-          customStyle="bg-gray-500"
+          action={() => setVariant('animals')}
+          customStyle='bg-gray-500'
         >
           Animals
         </CustomButton>
+        {variant === 'users' ? <CreateUserForm /> : <CreateAnimalForm />}
+
       </div>
-      <ul className="flex flex-col gap-3     ">
+      <ul className='flex flex-col gap-3     '>
         <Item variant={variant} data={appData} />
       </ul>
     </main>
