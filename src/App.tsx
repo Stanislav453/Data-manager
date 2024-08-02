d848031747b746ff6473dc229bf56ec7f5ee776a
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-02 08:28:51 +0200

    Add component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { Footer } from "./component/Footer";
+import { Header } from "./component/Header";
+import { ItemContainer } from "./component/ItemContainer";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <section>
+      <Header />
+      <ItemContainer />
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
