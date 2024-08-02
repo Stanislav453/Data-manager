92804e02b072a33716692a745d982ebdebe2dbe3
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-02 08:30:57 +0200

    create CustomButton

---
+++ src/component/CustomButton.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+type CustomButtonType = {
+  children: ReactNode;
+  customStyle?: string;
+  action?: () => void
+};
+
+export const CustomButton = ({ children, customStyle, action }: CustomButtonType) => {
+  return <button onClick={action} className={` py-2 px-4 text-white ${customStyle} rounded-full hover:scale-110 transition-transform `}>{children}</button>;
+};
