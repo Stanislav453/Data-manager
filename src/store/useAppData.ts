8fa41b005cd0d3b12e2d0b6b9194915721eb6e8f
Stanislav Jendrisak <jendrisak@procorp.cz>
2024-08-02 08:34:12 +0200

    Create state for filter data.

--- src/store/useAppData.ts
+++ src/store/useAppData.ts
@@ -3,14 +3,17 @@ import { type DataType } from "../type";
 
 type UseAppDataType = {
   data: DataType[];
+  filterData: DataType[];
   setData: (data: DataType[]) => void;
 };
 
 export const useAppData = create<UseAppDataType>()((set) => ({
   data: [],
+  filterData: [],
 
   setData: (data) =>
     set(() => ({
       data: data,
+      filterData: data,
     })),
 }));
