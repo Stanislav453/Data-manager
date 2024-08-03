0afe5b9543efab8f7a768926a93822b6592e320f
Stanislav Jendrišák <st.jendrisak@gmail.com>
2024-08-03 09:37:46 +0200

    create update func.

--- src/store/useAppData.ts
+++ src/store/useAppData.ts
@@ -1,11 +1,12 @@
-import { create } from "zustand";
-import { type DataType } from "../type";
+import { create } from 'zustand';
+import { type DataType } from '../type';
 
 type UseAppDataType = {
   data: DataType[];
   filterData: DataType[];
   setData: (data: DataType[]) => void;
   deleteData: (id: string) => void;
+  updateData: (item: DataType) => void;
 };
 
 export const useAppData = create<UseAppDataType>()((set) => ({
@@ -21,5 +22,10 @@ export const useAppData = create<UseAppDataType>()((set) => ({
   deleteData: (id) =>
     set((state) => ({
       filterData: state.filterData.filter((oneResult) => oneResult.id !== id),
+    })),
+
+  updateData: (item) =>
+    set((state) => ({
+      filterData: [...state.filterData, item],
     })),
 }));
