f8957b43f8f5cadb2ea57f0d1c1d12c253c5c0ec
Stanislav Jendrišák <st.jendrisak@gmail.com>
2024-08-04 13:40:01 +0200

    Create button variant.

--- src/component/ItemContainer.tsx
+++ src/component/ItemContainer.tsx
@@ -12,13 +12,19 @@ import { updateAction } from '../api/actions/updateAction';
 export const ItemContainer = () => {
   const [variant, setVariant] = useState('users');
   const appData = useAppData((state) => state.filterData);
+  const updateBan = useAppData((state) => state.updateBan);
+  const buttonVariantColor =
+    variant === 'users' ? 'bg-gray-500' : 'bg-gray-5100';
 
   useGetAction(variant);
   console.log(appData);
 
-  const handlleupdateItem = (id:string) => {
-    updateAction({variant, id})
-  }
+  const handlleupdateItem = (id: string) => {
+    const filterItem = appData.find((item) => item.id == id);
+    const newBan = { banned: !filterItem?.banned };
+    updateAction({ variant, id, values: newBan });
+    updateBan(id);
+  };
 
   return (
     <main className='w-full min-h-dvh py-5 px-3'>
@@ -26,7 +32,7 @@ export const ItemContainer = () => {
         <div className='flex gap-3'>
           <CustomButton
             action={() => setVariant('users')}
-            customStyle='bg-gray-500 self-end'
+            customStyle={`bg-gray-500 self-end`}
           >
             Users
           </CustomButton>
@@ -44,7 +50,11 @@ export const ItemContainer = () => {
         )}
       </div>
       <ul className='flex flex-col gap-3     '>
-        <Item variant={variant} data={appData} action={ (id) => handlleupdateItem(id)} />
+        <Item
+          variant={variant}
+          data={appData}
+          action={(id) => handlleupdateItem(id)}
+        />
       </ul>
     </main>
   );
