087b8f0e1339d4e69cb6de27c814fb10db2a0f2a
Stanislav Jendrišák <st.jendrisak@gmail.com>
2024-08-04 19:37:17 +0200

    Add props for type.

--- src/component/CustomButton.tsx
+++ src/component/CustomButton.tsx
@@ -1,20 +1,22 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 
 type CustomButtonType = {
   children: ReactNode;
   customStyle?: string;
   action?: () => void;
+  type?: "submit" | "button" 
 };
 
 export const CustomButton = ({
   children,
   customStyle,
   action,
+  type
 }: CustomButtonType) => {
   return (
     <button
       onClick={action}
-      type='submit'
+      type={type}
       className={` py-2 px-4 text-white ${customStyle} rounded-full hover:scale-110 transition-transform `}
     >
       {children}
