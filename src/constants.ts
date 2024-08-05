1e1bdc7ba64893ca7d4b9e5afad8bde4830f90a7
Stanislav Jendrišák <st.jendrisak@gmail.com>
2024-08-05 14:49:51 +0200

    create a constatnt for reusable text

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const DEFAULT = 'users';
+
+//FORMIK
+export const required = 'Required';
+export const onlyLetters = 'Only letters';
+export const minLetterName = 4;
+export const minLetterNameError = `Min ${minLetterName} characters`;
+export  const minAge = 1;
+export  const ageError = `Age cant be under ${minAge}`;
