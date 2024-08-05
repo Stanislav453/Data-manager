89d57f79d799f9c7a1e514199b6856784c68cc7e
Stanislav Jendrišák <st.jendrisak@gmail.com>
2024-08-05 14:51:52 +0200

    refactoring, add error if api not fetch

--- src/component/Item.tsx
+++ src/component/Item.tsx
@@ -8,26 +8,26 @@ import { useState } from 'react';
 import { IoCloseCircle } from 'react-icons/io5';
 import * as Yup from 'yup';
 import { updateAction } from '../api/actions/updateAction';
+import { required, onlyLetters, DEFAULT } from '../constants';
+import { ItemsFormType } from '../type';
+import { minAge, ageError } from '../constants';
 
 type ItemProps = {
   data: DataType[];
   variant: string;
+  error: unknown;
+  loading: boolean;
   action: (id: string) => void;
 };
-
-const minAge = 1;
-const ageError = 'Age cant be under 1';
-const nameError = 'No numbers';
-const required = 'required';
 
 const validationSchema = Yup.object().shape({
   name: Yup.string()
     .required(required)
-    .matches(/^[a-zA-Z\s]+$/, nameError),
+    .matches(/^[a-zA-Z\s]+$/, onlyLetters),
   age: Yup.number().min(minAge, ageError).required(required),
 });
 
-export const Item = ({ data, variant, action }: ItemProps) => {
+export const Item = ({ data, variant, loading, error, action }: ItemProps) => {
   const [editId, setEditId] = useState('');
   const updateData = useAppData((state) => state.deleteData);
   const editItem = useAppData((state) => state.editItem);
@@ -45,9 +45,19 @@ export const Item = ({ data, variant, action }: ItemProps) => {
     updateAction({ variant, id: editId, values: values });
     editItem(editId, values);
     setEditId('');
-    console.log('this is values');
   };
 
+  if(error){
+    return (
+      <p className='w-full text-center font-bold text-3xl text-red-500'>
+        Something is wrong. Please try it later.
+      </p>
+    );
+  }
+
+  if (loading) {
+    return <p className='w-full text-center font-bold text-3xl'>Loading...</p>;
+  }
   return data.map((oneResult, index) => {
     const { id, name, gender, banned, age, type } = oneResult;
 
@@ -70,15 +80,18 @@ export const Item = ({ data, variant, action }: ItemProps) => {
       >
         <div className='flex gap-2 w-full justify-center sm:justify-start'>
           {isEdit ? (
-            <Formik
+            <Formik<ItemsFormType>
               initialValues={
-                variant === 'animals' ? { name, age, type } : { name, gender }
+                variant === DEFAULT ? { name, gender } : { name, age, type }
               }
               validationSchema={validationSchema}
               onSubmit={handleSubmit}
             >
               {({ handleSubmit }) => (
-                <Form onSubmit={handleSubmit} className='flex gap-2 w-full'>
+                <Form
+                  onSubmit={handleSubmit}
+                  className='flex flex-wrap justify-center sm:justify-start gap-2 w-full'
+                >
                   <div className='flex flex-col'>
                     <Field
                       className='drop-shadow-md p-1.5 rounded-lg'
@@ -128,47 +141,60 @@ export const Item = ({ data, variant, action }: ItemProps) => {
                     </Field>
                   )}
                   <CustomButton customStyle='bg-orange-500'>
-                    Change item L{' '}
+                    Change item
                   </CustomButton>
                 </Form>
               )}
             </Formik>
           ) : (
             <>
-              <h2>
+              <div className='flex gap-2'>
                 <span className='font-bold'>Name: </span>
-                {name}
-              </h2>
+                <h2>{name}</h2>
+              </div>
               {gender && (
-                <p>
+                <div className='flex gap-2'>
                   <span className='font-bold'>Gender: </span>
-                  {gender}
-                </p>
+                  <h2>{gender}</h2>
+                </div>
               )}
               {age && (
-                <p>
+                <div className='flex gap-2'>
                   <span className='font-bold'>Age: </span>
-                  {age}
-                </p>
+                  <h2>{age}</h2>
+                </div>
               )}
               {type && (
-                <p>
+                <div className='flex gap-2'>
                   <span className='font-bold'>Type: </span>
-                  {type}
-                </p>
+                  <h2>{type}</h2>
+                </div>
               )}
             </>
           )}
         </div>
         <div className='flex'>
-          <CustomButton action={() => handleEditItem(id)} >
+          <CustomButton
+            action={() => {
+              id && handleEditItem(id);
+            }}
+          >
             {editIconVariant}
           </CustomButton>
-          <CustomButton action={() => handleDeleteData(id)}>
+          <CustomButton
+            action={() => {
+              id && handleDeleteData(id);
+            }}
+          >
             <FaRegTrashCan className='text-3xl text-red-500' />
           </CustomButton>
           {gender && (
-            <CustomButton customStyle={isBannedColor} action={() => action(id)}>
+            <CustomButton
+              customStyle={isBannedColor}
+              action={() => {
+                id && action(id);
+              }}
+            >
               {isBanned}
             </CustomButton>
           )}
